d1a44a2f44fb6df0fdcc6193ea22924c6a67fcb5
dnl <daleonpz@gmail.com>
2025-04-02 13:12:07 +0200

    add mcuboot example

---
+++ hello-world/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// #include <stdio.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+int main(void)
+{
+	LOG_INF("VERSION 0 - Hello World! %s", CONFIG_BOARD_TARGET);
+	return 0;
+}
