3d20298e88768b39487ad7a4c04fc2fe2e119693
Daniel P. <daleonpz@users.noreply.github.com>
2025-05-18 17:16:08 +0200

    Feat/ble sniffer (#4)
    * add licence and install ble_sniffer script
    
    * WIP dongle flashing
    
    * remove snifer instaler
    
    * remove script
    
    * remove code
    
    * remove unused dir
    
    * remove unused dir
    
    * remove warning flake.nix
    
    * add build script
    
    * update lock

--- flake.nix
+++ flake.nix
@@ -1,3 +1,16 @@
+# Copyright 2025 Daniel Paredes (daleonpz)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 {
     description = "Flake for Zephyr development";
 
@@ -60,7 +73,12 @@
             source .env/bin/activate
             export HOME=$(pwd)
             pip install west
-            west init
+            if [ -d ".west" ]; then
+              echo ".zephyr already initialized"
+            else
+              echo "Initializing zephyr"
+              west init
+            fi
             west update
             sh ~/scripts/install_zephyr_sdk.sh
             sh ~/scripts/install_stm32.sh
--- scripts/power_profiler_config.sh
+++ scripts/power_profiler_config.sh
@@ -1,3 +1,17 @@
+# Copyright 2025 Daniel Paredes (daleonpz)
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #!/bin/bash
 
 # Verify the last version of nRF Connect for Desktop
@@ -17,4 +31,3 @@ JLINK_URL="https://www.segger.com/downloads/jlink/${JLINK_FILE}"
 
 ###### MODIFY THIS VARIABLE TO CHANGE THE INSTALLATION DIRECTORY ###########
 JLINK_INSTALL_DIR="${HOME}/JLink"
-
